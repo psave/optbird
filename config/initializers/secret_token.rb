e9e93966a500123fefb341764e9ae0c5a2108cf3
psave <paulsave01@gmail.com>
2016-04-13 17:55:46 -0700

    initializing rails repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Optbird::Application.config.secret_key_base = '8a18aa4e3a67aaa6071a6c5e2ab3720d02622c1a4f2b81344734ede2cf79d809c3bf29dec01036029ad7a5a2b7152094e236398b3898484752e3c12a5a085e3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Optbird::Application.config.session_store :cookie_store, key: '_optbird_session'
