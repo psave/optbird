1c149daa35c072f828ffea4834aac12f6cfb4554
Alex Lindsay <alexjlindsay@gmail.com>
2016-04-16 00:50:45 +0000

    Update user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160414192419) do
 
-  create_table "users", id: false, force: true do |t|
-    t.integer "id",              null: false
-    t.string  "name"
-    t.string  "password_digest"
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
