89aa3d7755c921542cabf6b952b7b9bc1fd23f55
Alex Lindsay <alexjlindsay@gmail.com>
2016-04-17 16:49:51 +0000

    Add logout feature and logout link in footer.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,10 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root, notice: "Logged out."
+  end
+
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Optbird::Application.routes.draw do
 
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :charts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
