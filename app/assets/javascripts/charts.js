2f1163925db7b4498f005b22cb585135ccf0d916
Alex Lindsay <alexjlindsay@gmail.com>
2016-04-18 00:23:57 +0000

    Add range slider, but incorrect dates.

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -26,45 +26,52 @@ $(document).ready(function() {
       y_axis: y_axis,
       x_axis: x_axis
     };
+    console.log(series.x_axis);
 
     dataToChart();
   }
 
   function dataToChart() {
-    $('#container').highcharts({
+    $('#container').highcharts('StockChart', {
+      rangeSelector: {
+        selected: 1
+      },      
       title: {
-          text: 'Dummy Data',
-          x: -20 //center
+        text: 'Dummy Data',
+        x: -20 //center
       },
       subtitle: {
-          text: 'Dummy Data',
-          x: -20
+        text: 'Dummy Data',
+        x: -20
       },
       xAxis: {
-          categories: series.x_axis.map(function(time){ return moment(time).format("H:mm")}),
+        categories: series.x_axis.map(function(time){ return moment(time).format('MMMM Do YYYY, h:mm')}),
       },
       yAxis: {
-          title: {
-              text: 'Number of Occupants'
-          },
-          plotLines: [{
-              value: 0,
-              width: 1,
-              color: '#808080'
-          }]
+        title: {
+            text: 'Number of Occupants'
+        },
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }]
       },
       tooltip: {
-          valueSuffix: ''
+        valueSuffix: ''
       },
       legend: {
-          layout: 'vertical',
-          align: 'right',
-          verticalAlign: 'middle',
-          borderWidth: 0
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        borderWidth: 0
       },
       series: [{
         name: 'Occupants',
-        data: series.y_axis
+        data: series.y_axis,
+        tooltip: {
+          valueDecimals: 2
+        }
       }]
     });
   }
