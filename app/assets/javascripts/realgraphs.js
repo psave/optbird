ffc6e4a4548db2eb0d4dbbcf995ef4b1f1c182ed
fullerenedream <fullerenedream@gmail.com>
2016-04-21 18:46:38 +0000

    highchart working with real data from csv

--- app/assets/javascripts/realgraphs.js
+++ app/assets/javascripts/realgraphs.js
@@ -1,27 +1,38 @@
 $(document).ready(function() {
 
-  $('#button').on('click', function(event) {
-    event.preventDefault();
-    $.ajax({
-      method: 'GET',
-      url: '/realgraphs/show',
-      dataType: 'json',
-      contentType: 'application/json',
-      success: dataToArray
-    }); 
+  $.ajax({
+    method: 'GET',
+    url: '/realgraphs/show',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: dataToArray
   });
+  console.log("the line after the ajax request");
 
   var series = {};
 
   // TODO: response is everything in the occupants table -
   // you need to filter it in some way to make useful graphs
+
+  // try graphing all occupancy over time from one room
   function dataToArray(response) {
+    console.log("inside dataToArray");
     var x_axis = [];
     var y_axis = [];
+    console.log(response.length + " elements in response");
+    console.log(response[0]);
+    console.log("for i = 0: sample_time = " + response[0].sample_time);
+    console.log("for i = 0: number_occupants = " + response[0].number_occupants);
+    console.log("for i = 1: sample_time = " + response[1].sample_time);
+    console.log("for i = 1: number_occupants = " + response[1].number_occupants);
+    console.log("for i = 2: sample_time = " + response[2].sample_time);
+    console.log("for i = 2: number_occupants = " + response[2].number_occupants);
 
     for (var i = 0; i < response.length; i++) {
-      x_axis.push(response[i].date_time);
-      y_axis.push(response[i].occupants);
+      if (response[i].room_id == 2) {
+        x_axis.push(response[i].sample_time);
+        y_axis.push(response[i].number_occupants);
+      }
     }
 
     series = {
@@ -33,16 +44,14 @@ $(document).ready(function() {
   }
 
   function dataToChart() {
-    $('#container').highcharts('StockChart', {
-      rangeSelector: {
-        selected: 1
-      },      
+    console.log("inside dataToChart");
+    $('#graphContainer').highcharts({      
       title: {
-        text: 'Title Text',
+        text: 'Occupancy over Time',
         x: -20 //center
       },
       subtitle: {
-        text: 'Subtitle Text',
+        text: 'for room_id = 2',
         x: -20
       },
       xAxis: {
