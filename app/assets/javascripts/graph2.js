2b52013349095f75e823c8562324f96d9a4977b9
psave <paulsave01@gmail.com>
2016-04-26 22:18:58 -0700

    Providing averages along the Xaxis for the course and occupancy graph

--- app/assets/javascripts/graph2.js
+++ app/assets/javascripts/graph2.js
@@ -1,4 +1,17 @@
 // // /////////     This is the JS file for the graph currently on the "graph2" tab.
+function time_of_day(argument) {
+  var time_of_day = [];
+  for (var hour = 0; hour < 24; hour++){
+    for (var minute = 0; minute < 60; minute += 5 ){
+      var minuteString = String(minute);
+      if (minuteString.length == 1) {
+        minuteString = '0' + minuteString;
+      }
+      time_of_day.push(String(hour) + ":" + minuteString);
+    }
+  }
+  return time_of_day
+}
 
 function graph2(response) {
 
